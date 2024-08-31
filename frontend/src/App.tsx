3385aadab7a235542686276026c358bf9b23aa8e
biljeg <brunosiljeg@outlook.com>
2024-08-31 05:07:56 +0200

    feat: restarted project with vite

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,17 @@
+import { APIProvider, Map } from "@vis.gl/react-google-maps";
+import "./App.css";
+
 function App() {
   return (
-    <>
-      <h1 className="text-xl">Hello</h1>
-    </>
+    <APIProvider apiKey="AIzaSyDr-LVuWLuHDf58a2NBbfM5iuWb8WMqIyY">
+      <Map
+        style={{ width: "100vw", height: "100vh" }}
+        defaultCenter={{ lat: 22.54992, lng: 0 }}
+        defaultZoom={3}
+        gestureHandling={"greedy"}
+        disableDefaultUI={true}
+      />
+    </APIProvider>
   );
 }
 
