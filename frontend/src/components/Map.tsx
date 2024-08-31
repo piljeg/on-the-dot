4095e5d9a1e46848cab63a642bab9607666754f3
biljeg <brunosiljeg@outlook.com>
2024-08-31 09:38:19 +0200

    traffic map in progress

---
+++ frontend/src/components/Map.tsx
@@ -0,0 +1,56 @@
+import { Map, MapCameraChangedEvent } from "@vis.gl/react-google-maps";
+import PoiMarkers from "./PoiMarkers";
+import { Poi } from "../types";
+import { useTrafficLayer } from "../hooks/useTrafficLayer";
+
+const locations: Poi[] = [
+  {
+    key: "lucko",
+    location: { lat: 45.762160830063344, lng: 15.896473847306092 },
+  },
+  {
+    key: "trnje",
+    location: { lat: 45.7948013349046, lng: 15.979294093511566 },
+  },
+  {
+    key: "ivanja reka",
+    location: { lat: 45.80013180911717, lng: 16.131190012246865 },
+  },
+  {
+    key: "sredisce",
+    location: { lat: 45.777784456109195, lng: 15.990552774054418 },
+  },
+  {
+    key: "selska cesta",
+    location: { lat: 45.7865242106891, lng: 15.951781913539993 },
+  },
+];
+
+function CustomMap() {
+  const isTrafficLoaded = useTrafficLayer();
+
+  // if (!isTrafficLoaded) return <></>;
+
+  return isTrafficLoaded ? (
+    <Map
+      style={{ width: "100vw", height: "100vh" }}
+      defaultZoom={13}
+      mapId="febfb6882721eadd"
+      defaultCenter={{ lat: 45.815399, lng: 15.966568 }}
+      onCameraChanged={(ev: MapCameraChangedEvent) =>
+        console.log(
+          "camera changed:",
+          ev.detail.center,
+          "zoom:",
+          ev.detail.zoom,
+        )
+      }
+    >
+      <PoiMarkers pois={locations} />
+    </Map>
+  ) : (
+    <></>
+  );
+}
+
+export default CustomMap;
--- frontend/src/components/PoiMarkers.tsx
+++ frontend/src/components/PoiMarkers.tsx
@@ -1,9 +1,9 @@
+import { useCallback, useEffect, useRef, useState } from "react";
 import { AdvancedMarker, useMap } from "@vis.gl/react-google-maps";
 import { Poi } from "../types.ts";
 import CameraIcon from "../assets/icons/camera-icon.svg";
 import { MarkerClusterer } from "@googlemaps/markerclusterer";
 import type { Marker } from "@googlemaps/markerclusterer";
-import { useEffect, useRef, useState } from "react";
 
 const PoiMarkers = (props: { pois: Poi[] }) => {
   const [markers, setMarkers] = useState<{ [key: string]: Marker }>({});
@@ -26,6 +26,16 @@ const PoiMarkers = (props: { pois: Poi[] }) => {
     });
   };
 
+  const handleClick = useCallback(
+    (ev: google.maps.MapMouseEvent) => {
+      if (!map) return;
+      if (!ev.latLng) return;
+      console.log("marker clicked:", ev.latLng.toString());
+      map.panTo(ev.latLng);
+    },
+    [map],
+  );
+
   useEffect(() => {
     if (!map) return;
     if (!clusterer.current) {
@@ -45,6 +55,8 @@ const PoiMarkers = (props: { pois: Poi[] }) => {
           key={poi.key}
           position={poi.location}
           ref={marker => setMarkerRef(marker, poi.key)}
+          clickable={true}
+          onClick={handleClick}
         >
           <div>
             <img className="w-10 h-10" src={CameraIcon} alt="camera icon" />
