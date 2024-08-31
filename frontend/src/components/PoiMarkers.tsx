0e3f5b9ab4c13e46d6526ed691f630053412a617
biljeg <brunosiljeg@outlook.com>
2024-08-31 06:43:11 +0200

    feat: added cluster map

--- frontend/src/components/PoiMarkers.tsx
+++ frontend/src/components/PoiMarkers.tsx
@@ -1,20 +1,54 @@
-import { AdvancedMarker } from "@vis.gl/react-google-maps";
+import { AdvancedMarker, useMap } from "@vis.gl/react-google-maps";
 import { Poi } from "../types.ts";
 import CameraIcon from "../assets/icons/camera-icon.svg";
+import { MarkerClusterer } from "@googlemaps/markerclusterer";
+import type { Marker } from "@googlemaps/markerclusterer";
+import { useEffect, useRef, useState } from "react";
 
 const PoiMarkers = (props: { pois: Poi[] }) => {
+  const [markers, setMarkers] = useState<{ [key: string]: Marker }>({});
+  const clusterer = useRef<MarkerClusterer | null>(null);
+
+  const map = useMap();
+
+  const setMarkerRef = (marker: Marker | null, key: string) => {
+    if (marker && markers[key]) return;
+    if (!marker && !markers[key]) return;
+
+    setMarkers(prev => {
+      if (marker) {
+        return { ...prev, [key]: marker };
+      } else {
+        const newMarkers = { ...prev };
+        delete newMarkers[key];
+        return newMarkers;
+      }
+    });
+  };
+
+  useEffect(() => {
+    if (!map) return;
+    if (!clusterer.current) {
+      clusterer.current = new MarkerClusterer({ map });
+    }
+  }, [map]);
+
+  useEffect(() => {
+    clusterer.current?.clearMarkers();
+    clusterer.current?.addMarkers(Object.values(markers));
+  }, [markers]);
+
   return (
     <>
       {props.pois.map((poi: Poi) => (
-        <AdvancedMarker key={poi.key} position={poi.location}>
+        <AdvancedMarker
+          key={poi.key}
+          position={poi.location}
+          ref={marker => setMarkerRef(marker, poi.key)}
+        >
           <div>
             <img className="w-10 h-10" src={CameraIcon} alt="camera icon" />
           </div>
-          {/* <Pin
-            background={"#FBBC04"}
-            glyphColor={"#000"}
-            borderColor={"#000"}
-          /> */}
         </AdvancedMarker>
       ))}
     </>
